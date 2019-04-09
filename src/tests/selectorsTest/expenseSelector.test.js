e96667a40f76c9484b9e7e777cd2254184fb2eec
Fawad Bashir <bikerz.bashir@gmail.com>
2019-04-09 22:56:10 +0500

    Update expenseSelector.test.js

--- src/tests/selectorsTest/expenseSelector.test.js
+++ src/tests/selectorsTest/expenseSelector.test.js
@@ -1,29 +1,8 @@
 import selectExpenses from '../../selectors/expenses'
 import moment from 'moment'
+import expenses from '../fixtures/expensesFixture'
 
-const expenses = [
-    {
-        id: '1',
-        description: 'gum',
-        note: '',
-        amount: 100,
-        createdAt: moment(0).valueOf()
-    },
-    {
-        id: '2',
-        description: 'rent',
-        note: '',
-        amount: 109500,
-        createdAt: moment(0).subtract(4,'days').valueOf()
-    },
-    {
-        id: '3',
-        description: 'Credit Card',
-        note: '',
-        amount: 10950,
-        createdAt: moment(0).add(4,'days').valueOf()
-    }
-]
+
 
 
 
