b4d40447f9c764436c2722e1b4ac9bbdd82f061c
Fawad Bashir <bikerz.bashir@gmail.com>
2019-04-09 13:29:24 +0500

    Update filtersReducer.test.js

--- src/tests/reducerTests/filtersReducer.test.js
+++ src/tests/reducerTests/filtersReducer.test.js
@@ -27,3 +27,21 @@ test('should set sort By Date', () => {
     const state = filtersReducer(currentState, {type: 'SORT_BY_DATE'})
     expect(state.sortBy).toBe('date')
 })
+
+test('should set Filter by text', () => {
+    const text = 'This is my filter'
+    const state = filtersReducer(undefined, {type: 'SET_TEXT_FILTER',text})
+    expect(state.text).toBe(text)
+})
+
+test('should set startDate filter', () => {
+    const startDate = moment().startOf('month')
+    const state = filtersReducer(undefined, { type: 'SET_START_DATE',startDate })
+    expect(state.startDate).toBe(startDate)
+})
+
+test('should set EndDate filter', () => {
+    const endDate = moment().endOf('month')
+    const state = filtersReducer(undefined, { type: 'SET_END_DATE',endDate })
+    expect(state.endDate).toBe(endDate)
+})
