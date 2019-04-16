2dd77873dfbccf17131a64d78e9b04cb30cb8ca6
Fawad Bashir <bikerz.bashir@gmail.com>
2019-04-16 11:03:39 +0500

    Update ExpenseForm.test.js

--- src/tests/ExpenseForm.test.js
+++ src/tests/ExpenseForm.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import moment from 'moment'
 // import {createSerializer} from 'enzyme-to-json';
 import expenses from './fixtures/expensesFixture'
 import  ExpenseForm  from '../Components/ExpenseForm'
 // expect.addSnapshotSerializer(createSerializer({mode: 'shallow'}))
+
 
 test('should Render Expense Form correctly', () => {
     const wrapper = shallow(<ExpenseForm/>)
@@ -60,4 +62,26 @@ test('should set amount if inValid Input', () => {
         target: {value}
     })
     expect(wrapper.state('amount')).toBe('')
+})
+
+test('should call onSubmit prop for valid form submission', () => {
+    const onSubmitSpy = jest.fn()
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />)
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () =>{}
+    })
+    expect(wrapper.state('error')).toBe('')
+    expect(onSubmitSpy).toHaveBeenCalledWith({
+        description: expenses[0].description,
+        amount: expenses[0].amount,
+        note: expenses[0].note,
+        createdAt: expenses[0].createdAt
+    })
+})
+
+test('should set new State on DateChange', () => {
+    const now = moment()
+    const wrapper = shallow(<ExpenseForm/>)
+    wrapper.find('withStyles(SingleDatePicker)').prop('onDateChange')(now);
+    expect(wrapper.state('createdAt')).toEqual(now)
 })
