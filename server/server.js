d53a6369e9a555ee5526de4a9b29f0b72e18d744
Fawad Bashir <bikerz.bashir@gmail.com>
2019-04-26 00:28:35 +0500

    installed heroku

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const publicPath = path.join(__dirname, '..', public)
-
-app.use(express.static(publicPath))
-
-app.get('*', (req, res) => {
-res.sendFile(path.join(publicPath, 'index.html'))
-})
-
-app.listen(3000, () => {
-    console.log('Server is Up')
-})
+const express = require('express');
+// const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+const buildPath = path.join(__dirname, '..', 'build')
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(buildPath));
+app.use(express.static(path.join(buildPath)));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(buildPath , 'index.html'));
+});
+app.listen(port);
