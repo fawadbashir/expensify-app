128a3f80ae63abe4ca41863c345764d0f804a070
Fawad Bashir <bikerz.bashir@gmail.com>
2019-05-10 01:12:26 +0500

    setup redux-thunk and firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,21 +12,92 @@ const config = {
   };
   firebase.initializeApp(config);
 
-  firebase.database().ref().set({
-      name: 'Andrew Mead', 
-      age: 26,
-      isSingle: false,
-      location: {
-          city: 'Philadelphia',
-          country: 'United States'
-      },
-      
-  })
-
-  firebase.database().ref('age').set(27)
-
-  firebase.database().ref('attributes').set({
-      height: 73,
-      weight: 150
-  })
+  const database = firebase.database()
   
+  export { firebase, database as default }
+
+  // firebase.database().ref('expenses').push({
+  //   description: 'Coffee',
+  //   note:'expensive',
+  //   amount: 100,
+  //   createdAt: 1000,
+  // })
+
+  
+
+  // firebase.database().ref('expenses').once('value')
+  // .then((snapshot) => {
+  //   const expenses = []
+  //   snapshot.forEach((childSnapshot) => {
+  //     expenses.push({
+  //       id: childSnapshot.key,
+  //       ...childSnapshot.val()
+  //     })
+
+  //   })
+  //   console.log(expenses)
+  // })
+
+  // firebase.database().ref('expenses').on('value',((snapshot) => {
+  //   const expenses =[]
+  //    snapshot.forEach((childSnapshot) => {
+  //     expenses.push({
+  //       id: childSnapshot.key,
+  //       ...childSnapshot.val()
+  //     })
+  //   })
+  //   console.log(expenses)
+  // } ))
+
+  // firebase.database().ref('expenses').on('child_removed',((snapshot) => {
+  //   console.log(snapshot.key,snapshot.val())
+  // }))
+
+  // firebase.database().ref('expenses').on('child_changed', ((snapshot) => {
+  //   console.log('child changed',snapshot.key,snapshot.val())
+  // }))
+
+//   firebase.database().ref().set({
+//       name: 'Andrew Mead', 
+//       age: 26,
+//       stressLevel: 6,
+//       job: {
+//         title: 'Software developer',
+//         company: 'Google'
+//       },
+//       location: {
+//           city: 'Philadelphia',
+//           country: 'United States'
+//       },
+      
+//   })
+
+//   firebase.database().ref('age').set(27)
+
+//   firebase.database().ref('attributes').set({
+//       height: 73,
+//       weight: 150
+//   }).then(() => {
+//     console.log('Data Updated')
+// }).catch((e) => {
+//     console.log('Data Not Updated', e)
+// })
+  
+
+// firebase.database().ref('isSingle').remove().then(() => {
+//     console.log('Data Removed')
+// }).catch((e) => {
+//     console.log('Data Failed to Remove')
+// })
+
+// firebase.database().ref().on('value',((snapshot) => console.log(snapshot.val().name) ) )
+// firebase.database().ref().on('value', ((snapshot) =>{
+//   const val = snapshot.val()
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+// } ))
+
+// firebase.database().ref().update({
+//     // stressLevel: 9,
+//     'job/company' : 'Microsoft',
+//     'location/city': 'Seattle'
+// })
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import {Provider} from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import './App.scss'
 import configureStore from './store/configurestore'
-import {addExpense} from './actions/expenses'
+// import {addExpense} from './actions/expenses'
 // import {setTextFilter} from './actions/filters'
-import getVisibleExpenses from './selectors/expenses'
+// import getVisibleExpenses from './selectors/expenses'
 import * as serviceWorker from './serviceWorker'
 import './firebase/firebase'
 
@@ -14,19 +14,13 @@ const store = configureStore()
 
 
 
-store.dispatch(addExpense({description : 'water bill', amount : 4500000}))
-store.dispatch(addExpense({description : 'gas bill', createdAt : 1000}))
-store.dispatch(addExpense({description : 'Rent', amount : 109500}))
-
-store.dispatch(addExpense({description : 'bill'}))
-
 // store.dispatch(setTextFilter('water'))
 // setTimeout(() => {
 //     store.dispatch(setTextFilter('bill'))
 // }, 3000);
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
-console.log(visibleExpenses)
+// const state = store.getState()
+// const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
+// console.log(visibleExpenses)
 
 ReactDOM.render(<Provider store = {store}> <AppRouter/> </Provider>, document.getElementById('root'))
 
