22bc065bc73a98c702c9266f039f03f27593e13c
Fawad Bashir <bikerz.bashir@gmail.com>
2019-05-21 12:07:28 +0500

    setup asynchronous test case for fetching data

--- src/tests/actionsTests/expensesActions.test.js
+++ src/tests/actionsTests/expensesActions.test.js
@@ -1,13 +1,13 @@
 import thunk from 'redux-thunk'
 import configureMockStore from 'redux-mock-store'
-import { addExpense, editExpense, removeExpense, startAddExpense,setExpenses } from '../../actions/expenses'
+import { addExpense, editExpense, removeExpense, startAddExpense,setExpenses,startSetExpenses } from '../../actions/expenses'
 import expenses from '../fixtures/expensesFixture'
 import database from '../../firebase/firebase'
 
 beforeEach((done) => {
     const expenseData = {}
-    expenses.forEach(({id, description, amount, note, createdAt}) => {
-        expenses[id] = {description, amount, note, createdAt}
+    expenses.forEach(({ id, description, amount, note, createdAt}) => {
+        expenseData[id] = {description, amount, note, createdAt}
     })
     database.ref('expenses').set(expenseData).then(() => done())
 })
@@ -92,6 +92,18 @@ test('should set expense action object with data', () => {
     expect(action).toEqual({
         type: 'SET_EXPENSES',
         expenses
+    })
+})
+
+test('should fetch expenses from firebase', (done) => {
+    const store = createMockStore({})
+    store.dispatch(startSetExpenses()).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'SET_EXPENSES',
+            expenses
+        })
+        done()
     })
 })
 
