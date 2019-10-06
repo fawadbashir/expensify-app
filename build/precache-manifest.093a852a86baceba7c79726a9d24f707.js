0685faa9fa78a1ce7c9b2c600f40a5ff7e12433f
Fawad Bashir <31212077+fawadbashir@users.noreply.github.com>
2019-10-06 13:20:04 +0500

    adding to heroku

---
+++ build/precache-manifest.093a852a86baceba7c79726a9d24f707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc83370c1d73daa8d28b1115dcd048a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9263ca05023803ce0934",
+    "url": "/static/css/2.6d2c8d95.chunk.css"
+  },
+  {
+    "revision": "e53f2e3fe1ea3961abad",
+    "url": "/static/css/main.cd0dbc35.chunk.css"
+  },
+  {
+    "revision": "9263ca05023803ce0934",
+    "url": "/static/js/2.0d900754.chunk.js"
+  },
+  {
+    "revision": "e53f2e3fe1ea3961abad",
+    "url": "/static/js/main.e2fc40f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
