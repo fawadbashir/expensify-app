ccec60bef6d07da4897b716409e7f7ad593575a0
Fawad Bashir <31212077+fawadbashir@users.noreply.github.com>
2019-10-06 14:07:57 +0500

    deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const app = express();
 const buildPath = path.join(__dirname, '..', 'build')
 console.log(__dirname)
